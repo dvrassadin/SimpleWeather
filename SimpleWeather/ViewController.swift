6b1b57d66ccf00e98c32f51ebb1f7b556baec3d1
Daniil Rassadin <daniil@rassadin.org>
2025-05-12 23:18:43 +0600

    Initial Commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Daniil Rassadin on 12/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
